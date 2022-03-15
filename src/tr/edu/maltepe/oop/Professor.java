335924b83bc432301e19cd3155bd4458f618769b
mtunahand <dgrmnc32@gmail.com>
2022-03-15 11:02:57 +0300

    example

--- src/tr/edu/maltepe/oop/Professor.java
+++ src/tr/edu/maltepe/oop/Professor.java
@@ -1,5 +1,11 @@
 package tr.edu.maltepe.oop;
 
-public class Professor {
-    String name;
+public class Professor extends Person{
+
+
+    public void teaches(){
+
+        System.out.println("prof "+getName()+" is teaching");
+    }
+
 }
